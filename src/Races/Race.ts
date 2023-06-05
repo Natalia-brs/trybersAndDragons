7a5de1304d16932180af79b05c4c2164d9ceb54e
natalia <ntla.brs@gmail.com>
2023-06-05 15:41:43 -0300

    Requisito 2

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,6 +1,7 @@
 export default abstract class Race {
   private _name: string;
   private _dexterity: number;
+  protected static _instances = 0;
 
   constructor(name: string, dexterity: number) {
     this._name = name;
@@ -13,6 +14,10 @@ export default abstract class Race {
 
   get dexterity() {
     return this._dexterity;
+  }
+
+  protected static instances() {
+    this._instances += 1;
   }
 
   abstract get maxLifePoints(): number;
